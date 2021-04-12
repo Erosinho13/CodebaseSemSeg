e9344f8d7640c3733feb356eeac5c3d1f7482b56
rayquaza <eros.fani@gmail.com>
2021-04-12 11:38:24 +0200

    run script for bisenetv2

--- run.sh
+++ run.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=test
-#SBATCH --time=96:00:00
+#SBATCH --job-name=BSNV2_CTS
+#SBATCH --time=99:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task 2
+#SBATCH --cpus-per-task 8
 #SBATCH --partition=cuda
-#SBATCH --gres=gpu:1
-#SBATCH --mem=8GB
+#SBATCH --gres=gpu:2
+#SBATCH --mem=4GB
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=eros.fani@studenti.polito.it
+#SBATCH --mail-user=eros.fani@gmail.com
 ##
 # load cuda module
 module load nvidia/cudasdk/10.1
 python -m torch.distributed.launch --nproc_per_node=1 run.py \
---num_workers 2 \
+--model bisenetv2 \
+--num_workers 8 \
 --dataset cts \
---name default \
---lr 2.5e-3 \
---epochs 360 \
+--name BSNV2_CTS \
+--lr 5e-2 \
+--weight_decay 0.0005 \
+--epochs 690 \
 --val_interval 30 \
---hnm
+--hnm \
+--batch_size 16 \
+--ignore_warnings \
+--print \
+--output_aux \
+--no_pretrained
